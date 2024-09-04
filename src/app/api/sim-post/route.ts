4dfd4f077dcd69af368a911df0ff9c81de208fdb
AliAkram7 <soulimanali2323@gmail.com>
2024-09-04 11:48:37 +0100

    fix api

--- src/app/api/sim-post/route.ts
+++ src/app/api/sim-post/route.ts
@@ -1,9 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const body = await req.json();
+  const body = await req.text(); // Get the raw text body
+  const parsedBody = new URLSearchParams(body);
+  const formData: Record<string, string> = {}; // Define formData as an object with string keys and values
 
-  console.log(body);
+  for (const [key, value] of parsedBody.entries()) {
+    formData[key] = value;
+  }
+
+  // Now you can access the form data
+  console.log(formData);
 
   return NextResponse.json(
     { message: "success" },
